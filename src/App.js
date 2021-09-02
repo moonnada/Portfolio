a1063334db2928977c943a6b3d5fb86bd7388b8e
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-02 14:24:27 -0700

    imported all of files that i need

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import {Container, Grid} from '@material-ui/core';
+import React from 'react'
+
+import Profile from './components/Profile/Profile'
+import Header from './components/Header/Header'
+import Portfolio from './pages/Portfolio/Portfolio'
+import Resume from './pages/Resume/Resume'
+import Footer from './components/Footer/Footer'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      Our Resume clone
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12}lg={3} md={4} sm={12} style={{ backgroundColor: 'blue'}}>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{backgroundColor: 'red'}} >
+          <Header />
+          <Router>
+          <Switch>
+
+            <Route path='/portfolio'>
+              <Portfolio />
+            </Route>
+
+            <Route path='/resume'>
+              <Resume />
+
+            </Route>
+          </Switch>
+          </Router>
+          
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/pages/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div>
+            this is a resume page
+        </div>
+
+    )
+}
+
+export default Resume;

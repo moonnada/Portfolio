721d968b0d7bff9e5dff92cc0a76fe6fdb3c91f4
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-23 10:48:23 -0700

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './footer.css'
+import {Typography} from '@material-ui/core'
+import resumeData from "../../utils/resumeData"
 
 const Footer = () => {
     return (
-        <div>
-            This is a footer component
+        <div className='footer'>
+            <div className='footer_left'>
+                <Typography className='footer_name'>{resumeData.name}</Typography>  
+
+            </div>
+            <div className='footer-right'>
+                <Typography className='footer_copyright'>
+                    Designed and Developed by <a href="/" target="_blank">Ray Kim</a>
+                    <br/>
+                    
+                </Typography>
+            </div>
         </div>
 
     )

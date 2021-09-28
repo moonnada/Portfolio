f02ae129d05c162804d237666c36503879739c67
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-27 19:31:44 -0700

    updated copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import {Typography} from '@material-ui/core'
 import resumeData from "../../utils/resumeData"
 
 const Footer = () => {
+    
     return (
         <div className='footer'>
             <div className='footer_left'>
@@ -12,7 +13,8 @@ const Footer = () => {
             </div>
             <div className='footer-right'>
                 <Typography className='footer_copyright'>
-                    Designed and Developed by <a href="/" target="_blank">Ray Kim</a>
+                    Copyright @2021 <a href="/" target="_blank">Ray Kim</a>
+                     
                     <br/>
                     
                 </Typography>
